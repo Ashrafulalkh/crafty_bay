edaee1be8ccdcda03c5957bf5c60a24314676deb
Ashrafulalkh <ashrafulkhan194@gmail.com>
2024-09-15 13:00:42 +0600

    -added new screens and features

--- lib/presentation/ui/screens/splash_screen.dart
+++ lib/presentation/ui/screens/splash_screen.dart
@@ -1,5 +1,4 @@
-import 'package:crafty_bay/presentation/ui/screens/email_verification_screen.dart';
-import 'package:crafty_bay/presentation/ui/screens/home_screen.dart';
+import 'package:crafty_bay/presentation/ui/screens/main_bottom_nav_screen.dart';
 import 'package:crafty_bay/presentation/ui/widgets/app_logo.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -15,7 +14,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   Future<void> _moveToNextScreen() async{
     await Future.delayed(const Duration(seconds: 2),);
-    Get.off(()=> const HomeScreen());
+    Get.off(()=> const MainBottomNavScreen());
   }
 
   @override
